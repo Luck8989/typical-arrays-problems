10a7e535b465fc435f914977eb097231916ed52c
Tatsiana <t-tanja@bk.ru>
2020-10-01 23:23:05 +0300

    init: start Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.min.apply(Math, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    return Math.max.apply(Math, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) return 0;
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
 }
